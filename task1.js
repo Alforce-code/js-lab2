fba8de4f1e347af411fdbba272b4d5f52a80770a
bis22-tkalua@mubas.ac.mw <bis22-tkalua@mubas.ac.mw>
2025-08-05 17:32:43 +0200

    Added task1

---
+++ task1.js
@@ -0,0 +1,10 @@
+let num1;
+let num2;
+ num1=15;
+ num2 = 4;
+ console.log("The sum is equal to:", num1+num2)
+  console.log("The difference is equal to:", num1-num2)
+   console.log("The answer after division/quotient is equal to:", num1/+num2)
+    console.log("Their product is equal to:", num1*num2)
+     console.log("The remainder is:", num1%num2)
+      console.log("The sum is equal to:", num1+num2)

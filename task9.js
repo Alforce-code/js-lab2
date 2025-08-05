2009f5c9729c8101f42d4df7ebd6f50140bd4069
bis22-tkalua@mubas.ac.mw <bis22-tkalua@mubas.ac.mw>
2025-08-06 00:10:12 +0200

    Added the task9

--- task9.js
+++ task9.js
@@ -1,13 +1,20 @@
-const numbers = (num1,num2,num3)=>{
+const numbers = (num1, num2, num3) => {
     let greatest = num1;
-    if(num2>greatest){
-        greatest = num2
-    }else{
+
+    if (num2 > greatest) {
+        greatest = num2;
+    }
+
+    if (num3 > greatest) {
         greatest = num3;
     }
-}
+
+    return greatest;
+};
+
 const num1 = 10;
 const num2 = 20;
 const num3 = 15;
-numbers(num1,num2,num3);
-console.log(`The greatest number is: ${greatest}`);
+
+const result = numbers(num1, num2, num3);
+console.log(`The greatest number is: ${result}`);

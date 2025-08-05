9d12b6296ca32fffb9d9b7dc10f5871d099f2dd9
bis22-tkalua@mubas.ac.mw <bis22-tkalua@mubas.ac.mw>
2025-08-05 17:42:27 +0200

    Added the task2

---
+++ task2.js
@@ -0,0 +1,9 @@
+let isRaining = true;
+let hasUmbrella =false;
+if(isRaining && hasUmbrella){
+    console.log("It is raining, unfortunate you dont have an umbrella")
+}else if(isRaining || hasUmbrella){
+    console.log("true")
+}else if (!isRaining){
+console.log("Have a good day, enjoying some sunshine")
+}
